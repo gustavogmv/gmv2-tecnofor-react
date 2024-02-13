5b202bf519af4ca3a4a5d5ade09db0912179d969
Gustavo <gustsanc@gmail.com>
2024-02-13 10:44:17 +0100

    day02, use <react>Event handlers

--- components/src/components/Events.tsx
+++ components/src/components/Events.tsx
@@ -1,12 +1,28 @@
+import {ChangeEvent, MouseEvent} from "react";
+
 const Events = () => {
-    const handleClick = (ev: MouseEvent) => {
+    const handleClick = (ev: MouseEvent<HTMLButtonElement>) => {
         console.log('Button clicked!!!')
         console.log(ev)
     }
 
+    const handleChangeText = (ev: ChangeEvent<HTMLInputElement>) => {
+        console.log(ev.nativeEvent)
+    }
+
     return <div>
-        {/*note 'function definition' is provided (not execution)*/}
-        <button onClick={handleClick}>Click Here!</button>
+        <div>
+            {/*note 'function definition' is provided (not execution)*/}
+            <button onClick={handleClick}>Click Here!</button>
+            <button onClick={() => {
+                console.log('2nd button clicked!')
+            }}>Second button
+            </button>
+        </div>
+        <div>
+            {/*Evento sobre escritura en campo de texto*/}
+            <input type="text" onChange={handleChangeText}/>
+        </div>
     </div>
 }
 
