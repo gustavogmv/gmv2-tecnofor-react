a803efea5268bf58b07d4df90ee991f3de9d0034
Gustavo <gustsanc@gmail.com>
2024-02-13 12:33:50 +0100

    day02, state management example: add string

--- components/src/components/State.tsx
+++ components/src/components/State.tsx
@@ -3,14 +3,20 @@ import {useState, MouseEvent} from "react";
 
 const State = () => {
     const [counter, setCounter] = useState<number>(0)
+    const [msg, setMessage] = useState<string>("msg-")
+
 
     function hdlClick(ev: MouseEvent) {
-        setCounter(counter+1)
+        setCounter(counter + 1)
+        if (msg.length > 20) setMessage('msg-');
+        else setMessage(msg + '-')
     }
+
+    console.log("Render State....")
 
     return <div>
         <p>
-            Valor del contador: {counter}
+            Valor del contador: {counter} Message: {msg}
         </p>
         <button onClick={hdlClick}>Increment Counter</button>
     </div>
