38fd48a5268ed8197bb85142f066b0f2c7187e23
Gustavo <gustsanc@gmail.com>
2024-02-13 13:46:40 +0100

    day02, state management example: complex objects

--- components/src/components/Formulario.tsx
+++ components/src/components/Formulario.tsx
@@ -1,6 +1,6 @@
 // Modificar mediante 'useState' objetos complejos y ararys
 
-import {ChangeEvent, useState} from "react";
+import {ChangeEvent, FormEvent, useState} from "react";
 import Employee from "../interfaces/Employee.ts";
 import employee from "../interfaces/Employee.ts";
 
@@ -12,19 +12,25 @@ const Formulario = () => {
         const {value, name: fieldName} = ev.target
         // const key = fieldName
         // employeeCopy[key] = value
-        setEmployee({..._employee,[fieldName]: value})
+        setEmployee({..._employee, [fieldName]: value})
     }
 
-return <div>
-    {/*block default 'form' POST request*/}
-    <form>
-        <div><label>Name</label><input type="text" name="name" onChange={modifyfield}/></div>
-        <div><label>Surname</label><input type="text" name="surname" onChange={modifyfield}/></div>
-        <div><label>email</label><input type="text" name="email" onChange={modifyfield}/></div>
-        <div><label>Department</label><input type="text" name="department" onChange={modifyfield}/></div>
-        <div><label>Active</label><input type="text" name="active" onChange={modifyfield}/></div>
-    </form>
-</div>
+    const handleSubmit = (ev: FormEvent) => {
+        ev.preventDefault()
+        console.log(_employee)
+    }
+
+    return <div>
+        {/*block default 'form' POST request*/}
+        <form onSubmit={handleSubmit}>
+            <div><label>Name</label><input type="text" name="name" onChange={modifyfield}/></div>
+            <div><label>Surname</label><input type="text" name="surname" onChange={modifyfield}/></div>
+            <div><label>email</label><input type="text" name="email" onChange={modifyfield}/></div>
+            <div><label>Department</label><input type="text" name="department" onChange={modifyfield}/></div>
+            <div><label>Active</label><input type="text" name="active" onChange={modifyfield}/></div>
+            <input type="submit" value="Enviar"/>
+        </form>
+    </div>
 }
 
 export default Formulario
