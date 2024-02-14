229dde467d2445aa2610065591831184c938311e
Gustavo <gustsanc@gmail.com>
2024-02-14 13:59:14 +0100

    day03, use 'memo' on childre component List

--- hooks/src/components/FiltroAnimales.tsx
+++ hooks/src/components/FiltroAnimales.tsx
@@ -1,28 +1,36 @@
-import {useState} from "react";
+import {memo, useMemo, useState} from "react";
 
 type ListaProps = { items: string[] }
 
-const Lista = ({items}: ListaProps) => {
+// 'memo' allows rending List only when props changes!
+const Lista = memo(({items}: ListaProps) => {
     // delay 3 seconds
     const startTime = performance.now()
-    while(performance.now() - startTime < 1000){ /* empty */ }
+    while (performance.now() - startTime < 200) { /* empty */
+    }
 
     return <div>
         <ul>
-            {items.map((item)=>{return <li>{item}</li>})}
+            {items.map((item) => {
+                return <li>{item}</li>
+            })}
         </ul>
     </div>
-}
+})
 
 const FiltroAnimales = () => {
     const animales = ['perro', 'loro', 'leon', 'cocodrilo']
+    const [cont, setCont] = useState(0)
 
     const [busqueda, setBusqueda] = useState<string>('')
 
     // cost operation, filter animals list
-    const animalesFiltro = animales.filter((animal) => animal.includes(busqueda))
+    const animalesFiltro = useMemo(() => {
+        return animales.filter((animal) => animal.includes(busqueda))
+    }, [busqueda])
 
     return <div>
+        <button onClick={()=>{setCont(cont+1)}}> Increment  {cont} </button>
         <Lista items={animalesFiltro}/>
         <label>Filtro Animales: </label>
         <input type="text" onChange={(e) => {
