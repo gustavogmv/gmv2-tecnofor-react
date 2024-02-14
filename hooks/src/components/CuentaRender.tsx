7b5dbf1c32ac9d5af356b572530dd27ad5b45db9
Gustavo <gustsanc@gmail.com>
2024-02-14 13:01:15 +0100

    day03, useRef example: focus on html element

--- hooks/src/components/CuentaRender.tsx
+++ hooks/src/components/CuentaRender.tsx
@@ -1,16 +1,25 @@
 // count number of renders
 
-import {useEffect, useRef, useState} from "react";
+import {useRef, useState} from "react";
 
 const CuentaRender = () => {
     const [msg, setMsg] = useState('')
     const counter = useRef(0)
+    const inputRef = useRef<HTMLInputElement>(null)
 
-    counter.current = counter.current+1
+    counter.current = counter.current + 1
+
+    const handleFocusClick = () => {
+        inputRef.current?.focus()
+    }
 
     return <div style={{backgroundColor: "blanchedalmond"}}>
+        <p>{msg}</p>
         <p>{counter.current}</p>
-        <input type="text" onChange={(e)=>{setMsg(e.target.value)}}/>
+        <input ref={inputRef} type="text" onChange={(e) => {
+            setMsg(e.target.value)
+        }}/>
+        <button onClick={handleFocusClick}>@focus</button>
     </div>
 }
 
