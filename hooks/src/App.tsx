0a24953c6101aff6ca185a2f0984fcd37d56185c
Gustavo <gustsanc@gmail.com>
2024-02-14 11:56:31 +0100

    day03, clean-up: clean interval showing current date

--- hooks/src/App.tsx
+++ hooks/src/App.tsx
@@ -1,14 +1,18 @@
 import './App.css'
 import Efectos from "./components/Efectos.tsx";
 import Intervalo from "./components/Intervalo.tsx";
+import {useState} from "react";
 
 function App() {
     // const [count, setCount] = useState(0)
 
+    const [showDate, setShowDate] = useState(true)
+
     return (
         <>
+            <button onClick={()=>{setShowDate(!showDate)}}>{showDate} ? 'Hide' : 'Show'</button>
             <Efectos/>
-            <Intervalo/>
+            {showDate && <Intervalo/>}
         </>
     )
 }
--- hooks/src/components/Intervalo.tsx
+++ hooks/src/components/Intervalo.tsx
@@ -4,7 +4,13 @@ const Intervalo = () => {
     const [currentDate, setCurrentDate] = useState(new Date())
 
     useEffect(() => {
-        setInterval(()=>{setCurrentDate(new Date())}, 2000)
+        const interval = setInterval(() => {
+            setCurrentDate(new Date())
+            console.log("Interval...")
+        }, 500)
+        return () => {
+            clearInterval(interval)
+        }
     }, []);
     return <div>
         <p>{currentDate.toLocaleDateString()} {currentDate.getSeconds()} {currentDate.getMilliseconds()}</p>
