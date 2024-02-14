d60be1183ce42a852f2aae8d4702ab063776ade3
Gustavo <gustsanc@gmail.com>
2024-02-14 12:32:56 +0100

    day03, when not use effect: this is better!!

--- hooks/src/components/PrecioIva.tsx
+++ hooks/src/components/PrecioIva.tsx
@@ -1,14 +1,16 @@
 // example of when not to use 'useEffect'
-import {useEffect, useState} from "react";
+// import {useEffect, useState} from "react";
 
 type PrecioIvaProps = { precio: number }
 const PrecioIva = ({precio} : PrecioIvaProps) => {
 
-    const [precioIva, setPrecioIva] = useState(0)
+    // const [precioIva, setPrecioIva] = useState(0)
+    //
+    // useEffect(() => {
+    //     setPrecioIva(precio*1.21)
+    // }, []);
 
-    useEffect(() => {
-        setPrecioIva(precio*1.21)
-    }, []);
+    const precioIva = precio * 1.21
 
     return <div>
         <p>Precio IVA: {precioIva}</p>
