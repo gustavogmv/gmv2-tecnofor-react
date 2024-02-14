6c39ef61ee5560846edc60ae721ffa624d2e1119
Gustavo <gustsanc@gmail.com>
2024-02-14 10:47:49 +0100

    day03, hooks: 'useEffect' only gets called when 'deps' are modified
    When clicking button 'useEffect' is not called

--- hooks/src/components/Efectos.tsx
+++ hooks/src/components/Efectos.tsx
@@ -3,6 +3,7 @@ import {useEffect, useState} from "react";
 const Efectos = () => {
 
     const [mensaje, setMensaje] = useState('')
+    const [counter, setCounter] = useState(1)
 
     // Execute function after component renders
     useEffect(()=>{
@@ -10,13 +11,17 @@ const Efectos = () => {
          * 1- Despues de renderizar el componnente se ejecuta 'useEffect'
          * 2- Cuando se modifican las variables definidas como dependencia del 'useEffect' se
          * vuelve a ejecutar
+         * 3- Dentro de 'useEffect' podemos definir una funcion clean-up que se ejecuta siempre
+         * antes de la funcion del 'useEffect'
+         * 4- cuando el componente desaparece , tb se ejcuta la funcion de limpieza
          */
         console.log("component is already Rendered!")
-    })
+    }, [mensaje])
 
     return <div>
         <p>Mensaje del componente... {mensaje}</p>
         <input type="text" onChange={(e)=>{setMensaje(e.target.value)}} />
+        <button onClick={()=>{setCounter(counter+1)}}>Increment+{counter}</button>
     </div>
 }
 
