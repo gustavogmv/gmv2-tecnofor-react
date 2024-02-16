675cc22ace473934868984536d026224df501cee
Gustavo <gustsanc@gmail.com>
2024-02-16 13:37:10 +0100

    day05,[http] add 'POST' request, create Product

--- httpusage/src/components/ProductList.tsx
+++ httpusage/src/components/ProductList.tsx
@@ -7,33 +7,59 @@ import {Product} from "../interfaces/Product.ts";
 const ProductList = () => {
     const [products, setProducts] = useState<Array<Product>>([])
 
-    // useEffect(() => {
-    //     fetch(`https://peticiones.online/api/products`)
-    //         .then(response => response.json())
-    //         .then(data => setProducts(data.result))
-    // }, []);
-
     useEffect(() => {
-        async function getData() {
-            const response = await fetch(`https://peticiones.online/api/products`)
-            const data = await response.json()
-            setProducts(data.results)
-        }
-
-        getData()
-
+        fetch(`https://peticiones.online/api/products`)
+            .then(response => response.json())
+            .then(data => setProducts(data.results))
+            .catch(error => console.log(error))
     }, []);
 
-    return <div className="product-list">
-        {products.map((product) => {
-            return <div className="product">
-                <h2>{product.name}</h2>
-                <img src={product.image} alt={""}/>
-                <p>{product.description}</p>
-                <p>Precio: {product.price}</p>
-            </div>
-        })}
-    </div>
+    // 'async' mode!
+    // useEffect(() => {
+    //     async function getData() {
+    //         const response = await fetch(`https://peticiones.online/api/products`)
+    //         const data = await response.json()
+    //         setProducts(data.results)
+    //     }
+    //
+    //     getData()
+    //
+    // }, []);
+
+    const handleClick = async () => {
+        const newProduct: Product = {
+            _id: '',
+            name: 'Thermomix',
+            description: 'cooking',
+            price: 1000,
+            image: '',
+            category: 'cocina',
+            active: true
+        }
+        const response = await fetch(`https://peticiones.online/api/products`, {
+            method: 'POST',
+            headers: {'Content-Type': 'application/json'},
+            body: JSON.stringify(newProduct)
+        })
+        const data = await response.json()
+        console.log(data)
+    }
+
+    return <>
+        <div>
+            <button onClick={handleClick}>Create Product</button>
+        </div>
+        <div className="product-list">
+            {products.map((product) => {
+                return <div className="product">
+                    <h2>{product.name}</h2>
+                    <img src={product.image} alt={""}/>
+                    <p>{product.description}</p>
+                    <p>Precio: {product.price}</p>
+                </div>
+            })}
+        </div>
+    </>
 }
 
 export default ProductList;
