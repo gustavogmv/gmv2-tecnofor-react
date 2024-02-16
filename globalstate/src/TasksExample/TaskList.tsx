218bf8d81f7883102275169ac0435a0a4cb296ca
Gustavo <gustsanc@gmail.com>
2024-02-16 12:46:36 +0100

    day05, [globalstate]: use previous custom 'hooks' -> add delete button

--- globalstate/src/TasksExample/TaskList.tsx
+++ globalstate/src/TasksExample/TaskList.tsx
@@ -1,15 +1,22 @@
-import {useTaskContext} from "./TaskProvider.tsx";
+import {useDispatchContext, useTaskContext} from "./TaskProvider.tsx";
 
 const TaskList = () => {
     const tasks = useTaskContext()
+    const dispatch = useDispatchContext()
 
     return <div style={{backgroundColor: "lightseagreen"}}>
         <ul>
             {tasks && tasks.map((task) => {
                 return <li>
-                    <h3>{task.title}</h3>
-                    <p>{task.id}</p>
-                    <p>Category: {task.category}</p>
+                    <div style={{backgroundColor: "lightcoral"}}>
+                        <h3>{task.title}</h3>
+                        <p>{task.id}</p>
+                        <p>Category: {task.category}</p>
+                        <button onClick={() => {
+                            dispatch && dispatch({type: 'REMOVE', data: task.id})
+                        }} style={{backgroundColor:"indianred"}}>Delete
+                        </button>
+                    </div>
                 </li>
             })}
         </ul>
