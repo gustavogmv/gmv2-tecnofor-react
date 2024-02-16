376bc2af4ea3ff69080cc88d6d852ab007c60042
Gustavo <gustsanc@gmail.com>
2024-02-16 11:46:39 +0100

    day05, [globalstate] reducer example : add, remove elements

--- globalstate/src/App.tsx
+++ globalstate/src/App.tsx
@@ -1,11 +1,13 @@
 import './App.css'
 import Main from "./UseContextExample/Main.tsx";
+import ListaCompra from "./UseReduceExample/ListaCompra.tsx";
 
 function App() {
 
     return (
         <>
             <Main/>
+            <ListaCompra/>
         </>
     )
 }
---
+++ globalstate/src/UseReduceExample/ListaCompra.tsx
@@ -0,0 +1,56 @@
+import {useReducer, useRef} from "react";
+import {v4 as uuidv4} from "uuid";
+import {Simulate} from "react-dom/test-utils";
+import pointerDown = Simulate.pointerDown;
+
+interface Product {
+    id: string
+    name: string
+    available: boolean
+}
+
+type State = Array<Product>
+
+type Action =
+    | { type: "ADD", data: string }
+    | { type: "REMOVE", data: string }
+
+const reducer = (state: State, action: Action)  => {
+    switch (action.type) {
+        case "ADD": {
+            return [...state, {id: uuidv4(), name: action.data, available: true}]
+        }
+        case "REMOVE": {
+            return state.filter((product) => {
+                return product.id !== action.data
+            })
+        }
+    }
+}
+
+
+const ListaCompra = () => {
+    const [products, dispatch] = useReducer(reducer, [])
+
+    const inputRef = useRef<HTMLInputElement>(null)
+
+    const handleClick = () => {
+        dispatch({type: 'ADD', data: inputRef.current!.value})
+    }
+
+    return <div>
+        <label>Producto:</label>
+        <input type="text" ref={inputRef}/>
+        <button onClick={handleClick}>Create</button>
+        <div>
+            <ul>
+                {products.map(product => (
+                    <li key={product.id}
+                    onClick={()=>dispatch({type:'REMOVE',data:product.id})}>{product.name}</li>
+                ))}
+            </ul>
+        </div>
+    </div>
+}
+
+export default ListaCompra;
